3e3f88ee7b21958bf53b471481da1716f26dc3ed
Naman Kumar <namant744@gmail.com>
2022-03-18 00:14:45 +0530

    builder design pattern implementation

---
+++ src/creational/builder/Computer.java
@@ -0,0 +1,66 @@
+package creational.builder;
+
+public class Computer {
+
+    // required fields
+    private final String HDD;
+    private final String RAM;
+
+    // optional fields
+    private final Boolean isGraphicsCardEnabled;
+    private final Boolean isBluetoothEnabled;
+
+    public String getHDD() {
+        return HDD;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    public boolean isBluetoothEnabled() {
+        return isBluetoothEnabled;
+    }
+
+    private Computer(ComputerBuilder builder) {
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+
+    // Builder class
+    public static class ComputerBuilder {
+
+        // required fields
+        private String HDD;
+        private String RAM;
+
+        // optional fields
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String HDD, String RAM) {
+            this.HDD = HDD;
+            this.RAM = RAM;
+        }
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setBluetoothEnabled(boolean isBluetoothEnabled) {
+            this.isBluetoothEnabled = isBluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
